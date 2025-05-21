0252d6549aae8896f9c15ef538a10aa6d0405839
Paul Meyer <katexochen0@gmail.com>
2025-05-21 20:52:53 +0200

    news: add entry for mergiraf
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

---
+++ modules/misc/news/2025/05/2025-05-21_20-45-55.nix
@@ -0,0 +1,10 @@
+{
+  time = "2025-05-21T18:45:55+00:00";
+  condition = true;
+  message = ''
+    A new module is available: 'programs.mergiraf'.
+
+    Mergiraf can solve a wide range of Git merge conflicts based on syntax
+    awareness for many languages. See https://mergiraf.org/ for more information.
+  '';
+}
